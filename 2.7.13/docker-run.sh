83bf086e13a3ac11d7ed0a382cf080f18421d25f
iganari <cigaguri@gmail.com>
2017-08-07 16:49:39 +0900

    docker環境を整備

--- 2.7.13/docker-run.sh
+++ 2.7.13/docker-run.sh
@@ -5,4 +5,4 @@ set -x
 _TAG=python-2.7
 
 docker build . --tag ${_TAG}
-docker run --rm -it -v $(pwd)/src:/opt -w /opt ${_TAG} /bin/bash
+docker run --rm -it -v $(pwd)/src:/opt -w /opt --name="from-${_TAG}" ${_TAG} /bin/bash
