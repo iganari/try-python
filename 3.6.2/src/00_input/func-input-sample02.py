c7b4546c068d2cebdc858ff4af363ddd9ec05b2d
iganari <cigaguri@gmail.com>
2017-09-14 09:08:58 +0900

    sample02追加

---
+++ 3.6.2/src/00_input/func-input-sample02.py
@@ -0,0 +1,10 @@
+num01 = input("Please Enter num01: ")
+print(num01)
+num02 = input("Please Enter num02: ")
+print(num02)
+
+num01 = int(num01)
+num02 = int(num02)
+
+sum = num01 + num02
+print("The sum of num01 plus num02 is ",sum)
