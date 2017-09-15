1339a993a39831765530d2075082ccb9a34d8686
iganari <cigaguri@gmail.com>
2017-09-16 00:32:02 +0900

    rename

---
+++ 3.6.2/src/00_buildin/func-len-sample01.py
@@ -0,0 +1,3 @@
+print(len("hello"))
+print(len("12345678987654321"))
+print(len("huj88G%kjfdafs"))
--- 3.6.2/src/00_input/func-len-sample01.py
+++
@@ -1,7 +0,0 @@
-x = abcdefghijklmn
-y = AbcDeF
-z = 665599
-
-print(len(x))
-print(len(y))
-print(len(z))
